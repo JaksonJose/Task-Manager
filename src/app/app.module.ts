00fa2e1e98e1615f9eebba93f429fc96b77b8cfc
Jakson Jose <jaksonjosesilva34@gmail.com>
2022-01-27 22:55:59 -0300

    Added HTML for edit-task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksModule } from './tasks';
-import { EditTaskComponent } from './taks/edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditTaskComponent
   ],
   imports: [
     BrowserModule,
